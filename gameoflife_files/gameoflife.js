701025b280d3ba54c9cfbb0fd78e8b997a5fddec
Duy Nguyen <dnguyen790397@gmail.com>
2022-03-22 17:47:36 -0500

    Fix game of life

--- gameoflife_files/gameoflife.js
+++ gameoflife_files/gameoflife.js
@@ -107,7 +107,7 @@ var Shape = mClass(function () {
                     name: "Tumbler",
                     data: [[0, 3], [0, 4], [0, 5], [1, 0], [1, 1], [1, 5], [2, 0], [2, 1], [2, 2], [2, 3], [2, 4], [4, 0], [4, 1], [4, 2], [4, 3], [4, 4], [5, 0], [5, 1], [5, 5], [6, 3], [6, 4], [6, 5]]
                 },
-                {name: "Clear", data: []}
+                {name: "Create Your Own (Click Anywhere Above!) ", data: []}
 
             ];
         },
