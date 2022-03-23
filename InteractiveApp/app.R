b87352a5fba42287a8693088b31dc30616b95bd5
Duy Nguyen <dnguyen790397@gmail.com>
2022-03-23 09:03:31 -0500

    Update shiny app

--- InteractiveApp/app.R
+++ InteractiveApp/app.R
@@ -1,42 +1,59 @@
 library(shiny)
 library(tidyverse)
 library(ggplot2)
+library(ggpmisc)
 
 ui <- fluidPage(
-  tags$head(
-    tags$style(HTML('.shiny-split-layout>div {overflow: hidden;}')),
-  ),
-  tagList(
-    
-    br(), br(),
-    column(4,
-           wellPanel(
-             # Input: Select a file ----
-             fileInput("file", "Import Your CSV",
-                       multiple = TRUE,
-                       accept = c("text/csv",
-                                  "text/comma-separated-values,text/plain",
-                                  ".csv")),
-             #tags$hr(),
-             #tags$hr(),
-             radioButtons("plotType", "Select Plot Type",
-                          choices = c("Histogram", "Boxplot", "Scatterplot", "Density")),
-             checkboxInput("showRegLine", "Show Regression (Scatterplot)", TRUE),
-             selectInput("variable", "Select Variable", choices = NULL),
-             sliderInput("bins", "Number of Bins", min = 1, max = 50, value = 30)
-           )
+  sidebarLayout(
+    sidebarPanel(
+      conditionalPanel(condition = "input.tabs == 'Plot'",
+                       selectInput("myData", "Choose From My Datasets: (under dev)",
+                                   choices = c("rock", "pressure", "cars")),
+                       fileInput("file", "Or Upload Your Own CSV!", multiple = TRUE,
+                                 accept = c("text/csv", "text/comma-separated-values,text/plain", ".csv")),
+                       selectInput("explanatory", "Select Explanatory", choices = NULL),
+                       
+                       radioButtons("plotType", "Select Plot Type",
+                                    choices = c("Scatterplot", "Histogram", "Boxplot")),
+                       conditionalPanel(condition = "input.plotType == 'Histogram'",
+                                        sliderInput("bins", "Adjust Bins", min = 1, max = 50, value = 30),
+                                        checkboxInput("showDensity", "Show Density Plot", TRUE)),
+                       conditionalPanel(condition = "input.plotType == 'Scatterplot'",
+                                        selectInput("response", "Select Response", choices = NULL),
+                                        checkboxInput("showRegLine", "Show Regression Line", TRUE))
+      ),
+      conditionalPanel(condition = "input.tabs =='Table'",
+                       checkboxInput("removeNA", "Remove NAs", TRUE)
+      )
     ),
-    column(8, plotOutput("plot"))
+    mainPanel(
+      tabsetPanel(id="tabs",
+                  tabPanel("Plot",
+                           plotOutput("plot")),
+                  tabPanel("Table",
+                           tableOutput("table")),
+      )
+    )
   )
 )
 
+iris = data(iris)
+pressure = data(pressure)
+mtcars = data(mtcars)
+
 server <- function(input, output,session) {
   
-  # get data from file
+  
   data <- reactive({
-    req(input$file)
+    # switch(input$myData,
+    #        "rock" = iris,
+    #        "pressure" = pressure,
+    #        "cars" = mtcars)
     
-    # as shown in the book, lets make sure the uploaded file is a csv
+    req(input$file)
+    #req(input$myData)
+    
+    # get data from file
     ext <- tools::file_ext(input$file$name)
     validate(need(ext == "csv", "Invalid file. Please upload a .csv file"))
     
@@ -48,34 +65,71 @@ server <- function(input, output,session) {
     dataset
   })
   
+  output$table <- renderTable({
+    data()
+  })
+  
   # create the select input based on the numeric columns in the dataframe
   observeEvent( input$file, {
     req(data())
     num_cols <- dplyr::select_if(data(), is.numeric)
-    updateSelectInput(session, "variable", choices = colnames(num_cols))
+    updateSelectInput(session, "explanatory", choices = colnames(num_cols))
   })
+  observeEvent( input$file, {
+    req(data())
+    num_cols <- dplyr::select_if(data(), is.numeric)
+    updateSelectInput(session, "response", choices = colnames(num_cols))
+  })
+  # observeEvent( input$myData, {
+  #   req(data())
+  #   num_cols <- dplyr::select_if(data(), is.numeric)
+  #   updateSelectInput(session, "explanatory", choices = colnames(num_cols))
+  # })
+  # observeEvent( input$myData, {
+  #   req(data())
+  #   num_cols <- dplyr::select_if(data(), is.numeric)
+  #   updateSelectInput(session, "response", choices = colnames(num_cols))
+  # })
   
   # plot histogram
   output$plot <- renderPlot({
-    #req(!is.null(input$variable))
+    req(!is.null(input$explanatory))
     
     if( input$plotType == "Histogram"){
-      ggplot(data()) + aes_string(x = input$variable) +
-        geom_histogram(bins = input$bins)
-    } else if( input$plotType == "Boxplot") {
-      ggplot(data()) + aes_string(x = input$variable) +
-        geom_boxplot(outlier.color = "red", outlier.shape = 1)
-    } else if( input$plotType == "Scatterplot") {
-      p1 = ggplot(data(), aes(IBU, ABV)) + geom_point()
-      coefs = lm(ABV~IBU, data = data())$coefficients
-      p2 = {if( input$showRegLine)
-        p1 + geom_smooth(method = "lm", color = "red")
+      #########################################################    
+      p1 = ggplot(data(), aes_string(x = input$explanatory)) +
+        theme_bw(base_size = 16) +
+        geom_histogram(aes(y = ..density..), color = "black", fill = "white", bins = input$bins)
+      p2 = {if( input$showDensity)
+        p1 + geom_density(alpha = .2, fill = "#FF6666")
         else p1}
       print(p2)
-    } else if( input$plotType == "Density") {
-      ggplot(data()) + aes_string(x = input$variable) +
-        geom_density(alpha=0.8)
-        #geom_density(aes(fill=factor(cyl)), alpha=0.8)
+      #########################################################    
+    } else if( input$plotType == "Boxplot") {
+      ggplot(data()) + aes_string(x = input$explanatory) +
+        theme_bw(base_size = 16) +
+        geom_boxplot(outlier.color = "red", outlier.shape = 1, fill = "yellow")
+    } else if( input$plotType == "Scatterplot") {
+      myFormula = y ~ x
+      p1 = ggplot(data(), aes_string(x = input$explanatory, y = input$response)) + 
+        geom_point() +
+        theme_bw(base_size = 16)
+      p2 = {if( input$showRegLine)
+        p1 + geom_smooth(method = "lm", color = "red", formula = myFormula) +
+          stat_fit_tb(method = "lm",
+                      method.args = list(formula = myFormula),
+                      tb.vars = c(Parameter = "term", 
+                                  Estimate = "estimate", 
+                                  "StdErr" = "std.error", 
+                                  "italic(t)" = "statistic", 
+                                  "italic(P)" = "p.value"),
+                      label.y = "bottom", label.x = "right",
+                      parse = TRUE) +
+          stat_poly_eq(aes(label = paste0("atop(", ..eq.label.., ",", ..rr.label.., ")")), 
+                       formula = myFormula, 
+                       parse = TRUE)
+        else p1}
+      print(p2)
     }
   })
   
