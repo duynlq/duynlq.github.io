f7658a74b3ef9ece2b3c6a282f2218cac956c784
Duy Nguyen <58082234+epicduy@users.noreply.github.com>
2022-12-04 16:17:22 -0600

    sliding puzzle 5x5

--- assets/vendor/sliderpuzzle_files/script.js
+++ assets/vendor/sliderpuzzle_files/script.js
@@ -7,11 +7,11 @@ function swapTiles(cell1,cell2) {
 
 function shuffle() {
 //Use nested loops to access each cell of the 3x3 grid
-for (var row=1;row<=3;row++) { //For each row of the 3x3 grid
-   for (var column=1;column<=3;column++) { //For each column in this row
+for (var row=1;row<=5;row++) { //For each row of the 3x3 grid
+   for (var column=1;column<=5;column++) { //For each column in this row
   
-    var row2=Math.floor(Math.random()*3 + 1); //Pick a random row from 1 to 3
-    var column2=Math.floor(Math.random()*3 + 1); //Pick a random column from 1 to 3
+    var row2=Math.floor(Math.random()*5 + 1); //Pick a random row from 1 to 3
+    var column2=Math.floor(Math.random()*5 + 1); //Pick a random column from 1 to 3
      
     swapTiles("cell"+row+column,"cell"+row2+column2); //Swap the look & feel of both cells
   } 
@@ -21,31 +21,31 @@ for (var row=1;row<=3;row++) { //For each row of the 3x3 grid
 function clickTile(row,column) {
   var cell = document.getElementById("cell"+row+column);
   var tile = cell.className;
-  if (tile!="tile9") { 
+  if (tile!="tile25") { 
        //Checking if white tile on the right
-       if (column<3) {
-         if ( document.getElementById("cell"+row+(column+1)).className=="tile9") {
+       if (column<5) {
+         if ( document.getElementById("cell"+row+(column+1)).className=="tile25") {
            swapTiles("cell"+row+column,"cell"+row+(column+1));
            return;
          }
        }
        //Checking if white tile on the left
        if (column>1) {
-         if ( document.getElementById("cell"+row+(column-1)).className=="tile9") {
+         if ( document.getElementById("cell"+row+(column-1)).className=="tile25") {
            swapTiles("cell"+row+column,"cell"+row+(column-1));
            return;
          }
        }
          //Checking if white tile is above
        if (row>1) {
-         if ( document.getElementById("cell"+(row-1)+column).className=="tile9") {
+         if ( document.getElementById("cell"+(row-1)+column).className=="tile25") {
            swapTiles("cell"+row+column,"cell"+(row-1)+column);
            return;
          }
        }
        //Checking if white tile is below
-       if (row<3) {
-         if ( document.getElementById("cell"+(row+1)+column).className=="tile9") {
+       if (row<5) {
+         if ( document.getElementById("cell"+(row+1)+column).className=="tile25") {
            swapTiles("cell"+row+column,"cell"+(row+1)+column);
            return;
          }
